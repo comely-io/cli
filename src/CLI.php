8bcb5117a9a18f852bc4177541bacd9354e7943d
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-12-23 05:19:52 +0500

    version

--- src/CLI.php
+++ src/CLI.php
@@ -25,9 +25,9 @@ use Comely\Utils\OOP\OOP;
 class CLI
 {
     /** string Version (Major.Minor.Release-Suffix) */
-    public const VERSION = "1.0.22";
+    public const VERSION = "1.0.23";
     /** int Version (Major * 10000 + Minor * 100 + Release) */
-    public const VERSION_ID = 10022;
+    public const VERSION_ID = 10023;
 
     /** @var Directory */
     private $dir;

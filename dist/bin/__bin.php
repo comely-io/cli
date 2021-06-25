69e328b7d3b61d66f234692d1b308c1aee2d3a0b
Furqan Siddiqui <hello@furqansiddiqui.com>
2021-06-25 12:25:26 +0500

    dist

--- dist/bin/__bin.php
+++ dist/bin/__bin.php
@@ -1,5 +1,4 @@
-<?php /** @noinspection PhpUnhandledExceptionInspection */
-/**
+<?php /*
  * This file is a part of "comely-io/cli" package.
  * https://github.com/comely-io/cli
  *
@@ -8,8 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code or visit following link:
  * https://github.com/comely-io/cli/blob/master/LICENSE
- */
-
+ */ /** @noinspection PhpUnhandledExceptionInspection */
 declare(strict_types=1);
 
 require "../../vendor/autoload.php";
